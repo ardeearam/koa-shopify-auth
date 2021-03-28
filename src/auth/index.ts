4c1f49ec64f64d20fe4f267a385800e60f1f180a
Ardee Aram <ardee.aram@gmail.com>
2021-03-28 17:58:27 +0000

    WIP

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -32,7 +32,7 @@ function shouldPerformInlineOAuth({cookies}: Context) {
 export default function createShopifyAuth(options) {
   
   if (options.contextInitialParams) {
-    console.log("Initializing Shopify.Context in createShopifyAuth as workaround");
+    console.log("Initializing Shopify.Context in createShopifyAuth as workaround v1");
     Shopify.Context.initialize(options.contextInitialParams);  
   }
   
